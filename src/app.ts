51f9154696b12f330e8348e4a999880003e339f2
jhone-cmd <jhone.1941@gmail.com>
2025-12-02 09:52:45 -0300

    processing created batches

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,11 @@ import { createReadStream } from 'node:fs';
 import path, { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import express from 'express';
-import { produtosSimilares, todosProdutos } from './db.ts';
+import {
+  produtosSimilares,
+  setarEmbeddingProduto,
+  todosProdutos,
+} from './db.ts';
 import {
   createEmbeddingBatch,
   createEmbeddingBatchFile,
@@ -11,8 +15,7 @@ import {
   generateCart,
   generateEmbedding,
   generateProducts,
-  getBatch,
-  getBatchFileContent,
+  processEmbeddingsBatch,
   uploadFile,
 } from './openai.ts';
 
@@ -98,15 +101,24 @@ app.post('/embedding-batch', async (_, res) => {
 });
 
 app.post('/embedding-batch/results', async (_, res) => {
-  const batch = await getBatch('batch-as5d68as9d8afg8d79s564');
-
-  if (batch.status !== 'completed' || !batch.output_file_id) {
-    res.status(200).json(batch);
+  const result = await processEmbeddingsBatch('batch-asd45a6g5df54g64dsf8s64');
+  if (!result) {
+    res.status(200).json({ message: 'Still processing' });
     return;
   }
 
-  // console.log('TODO: process results', batch.output_file_id);
-  const file = await getBatchFileContent(batch.output_file_id);
+  result.forEach((product) =>
+    setarEmbeddingProduto(product.id, product.embeddings)
+  );
 
-  res.status(200).json(file);
+  res.status(200).end();
+});
+
+app.get('/products', async (_, res) => {
+  const products = todosProdutos().map((product) => ({
+    ...product,
+    embedding: product.embedding ? product.embedding.slice(0, 3) : null,
+  }));
+
+  res.status(200).json(products);
 });
